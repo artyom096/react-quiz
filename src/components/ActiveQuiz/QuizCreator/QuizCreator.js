5da6bb83803fdcb46d5b1337aa9fcd85c06d0e12
artyom096 <elekinshik@gmail.com>
2021-01-08 23:13:19 +0300

    feat: add select

--- src/components/ActiveQuiz/QuizCreator/QuizCreator.js
+++ src/components/ActiveQuiz/QuizCreator/QuizCreator.js
@@ -4,6 +4,7 @@ import classes from './QuizCreator.module.css'
 import Button from './../UI/Button'
 import {createControl} from './../../../form/formFramework'
 import Input from './../UI/Input/Input'
+import Select from '../UI/Select/Select';
 
 function createOptionControl(number) {
     return createControl({
@@ -30,6 +31,7 @@ class QuizCreator extends Component {
 
     state = {
         quiz: [],
+        rightAnswer: 1,
         formControls: createFormControls()
     }
 
@@ -70,7 +72,26 @@ class QuizCreator extends Component {
         })
     }
 
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswer: event.target.value
+        })
+    }
+
     render() {
+
+        const select = <Select
+        label='Выберите правильный ответ'
+        value={this.state.rightAnswer}
+        onChange={this.selectChangeHandler}
+        options={[
+            {text: 1, value: 1},
+            {text: 2, value: 2},
+            {text: 3, value: 3},
+            {text: 4, value: 4},
+        ]}
+        />
+
         return (
             <div className={classes.QuizCreator}>
                 <div >
@@ -79,7 +100,7 @@ class QuizCreator extends Component {
 
                         {this.renderControls()}
 
-                        <select></select>
+                        { select }
 
                         <Button
                             onClick={this.addQuestionHandler}
