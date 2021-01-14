a75726c2ae024dc713ba696a2b6bfa97f61d56a5
artyom096 <elekinshik@gmail.com>
2021-01-14 19:58:41 +0300

    feat: add redux for Quiz

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -3,69 +3,21 @@ import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 import Loader from '../../components/UI/Loader/Loader'
-import {fetchQuizById} from '../../store/actions/quiz'
+import { fetchQuizById, quizAnswerClick, retryQuiz } from '../../store/actions/quiz'
 import { connect } from 'react-redux'
 
 class Quiz extends Component {
 
-  onAnswerClickHandler = answerId => {
-    if (this.state.answerState) {
-      const key = Object.keys(this.state.answerState)[0]
-      if (this.state.answerState[key] === 'success') {
-        return
-      }
-    }
+  componentDidMount() {
+    this.props.fetchQuizById(this.props.match.params.id)
+  }
 
-    const question = this.state.quiz[this.state.activeQuestion]
-    const results = this.state.results
-
-    if (question.rightAnswerId === answerId) {
-      if (!results[question.id]) {
-        results[question.id] = 'success'
-      }
-
-      this.setState({
-        answerState: {[answerId]: 'success'},
-        results
-      })
-
-      const timeout = window.setTimeout(() => {
-        if (this.isQuizFinished()) {
-          this.setState({
-            isFinished: true
-          })
-        } else {
-          this.setState({
-            activeQuestion: this.state.activeQuestion + 1,
-            answerState: null
-          })
-        }
-        window.clearTimeout(timeout)
-      }, 1000)
-    } else {
-      results[question.id] = 'error'
-      this.setState({
-        answerState: {[answerId]: 'error'},
-        results
-      })
-    }
+  componentWillUnmount(){
+    this.props.retryQuiz()
   }
 
   isQuizFinished() {
     return this.state.activeQuestion + 1 === this.state.quiz.length
-  }
-
-  retryHandler = () => {
-    this.setState({
-      activeQuestion: 0,
-      answerState: null,
-      isFinished: false,
-      results: {}
-    })
-  }
-
-  componentDidMount() {
-    this.props.fetchQuizById(this.props.match.params.id)
   }
 
   render() {
@@ -76,22 +28,21 @@ class Quiz extends Component {
 
           {
             this.props.loading || !this.props.quiz
-             ? <Loader />
-             : this.props.isFinished
-              ? <FinishedQuiz
-                results={this.props.results}
-                quiz={this.props.quiz}
-                onRetry={this.retryHandler}
-              />
-              : <ActiveQuiz
-                answers={this.props.quiz[this.props.activeQuestion].answers}
-                question={this.props.quiz[this.props.activeQuestion].question}
-                onAnswerClick={this.onAnswerClickHandler}
-                quizLength={this.props.quiz.length}
-                answerNumber={this.props.activeQuestion + 1}
-                state={this.props.answerState}
-              />
-
+              ? <Loader />
+              : this.props.isFinished
+                ? <FinishedQuiz
+                  results={this.props.results}
+                  quiz={this.props.quiz}
+                  onRetry={this.props.retryQuiz}
+                />
+                : <ActiveQuiz
+                  answers={this.props.quiz[this.props.activeQuestion].answers}
+                  question={this.props.quiz[this.props.activeQuestion].question}
+                  onAnswerClick={this.props.quizAnswerClick}
+                  quizLength={this.props.quiz.length}
+                  answerNumber={this.props.activeQuestion + 1}
+                  state={this.props.answerState}
+                />
           }
         </div>
       </div>
@@ -112,7 +63,9 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetchQuizById: id => dispatch(fetchQuizById(id))
+    fetchQuizById: id => dispatch(fetchQuizById(id)),
+    quizAnswerClick: answerId => dispatch(quizAnswerClick(answerId)),
+    retryQuiz: () => dispatch(retryQuiz())
   }
 }
 
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,5 +1,12 @@
 
-import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZ_ERROR, FETCH_QUIZ_START, FETCH_QUIZ_SUCCESS } from "../actions/actionsType"
+import { FETCH_QUIZES_ERROR, 
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY,
+    QUIZ_SET_STATE } from "../actions/actionsType"
 
 const initialState = {
     quizes: [],
@@ -10,27 +17,48 @@ const initialState = {
     activeQuestion: 0,
     answerState: null,
     quiz: null
-  }
-  
-  export default function quizReducer(state = initialState, action) {
+}
+
+export default function quizReducer(state = initialState, action) {
     switch (action.type) {
-      case FETCH_QUIZES_START:
-        return {
-          ...state, loading: true
-        }
-      case FETCH_QUIZES_SUCCESS:
-        return {
-          ...state, loading: false, quizes: action.quizes
-        }
-      case FETCH_QUIZES_ERROR:
-        return {
-          ...state, loading: false, error: action.error
-        }
-      case FETCH_QUIZ_SUCCESS:
-        return {
-          ...state, loading: false, quiz: action.quiz
-        }
-      default:
-        return state
+        case FETCH_QUIZES_START:
+            return {
+                ...state, loading: true
+            }
+        case FETCH_QUIZES_SUCCESS:
+            return {
+                ...state, loading: false, quizes: action.quizes
+            }
+        case FETCH_QUIZES_ERROR:
+            return {
+                ...state, loading: false, error: action.error
+            }
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state, loading: false, quiz: action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state, answerState: action.answerState,
+                results: action.results
+            }
+        case FINISH_QUIZ:
+            return {
+                ...state, isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state, answerState: null, activeQuestion: action.number
+            }
+        case QUIZ_RETRY:
+            return{
+                ...state, activeQuestion: 0, 
+                answerState: null, 
+                isFinished: false, 
+                results: {}
+
+            }
+        default:
+            return state
     }
-  }
+}
